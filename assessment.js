a244072311dabe5c643d54e8f8ecce28fd272f11
Tanner Bryant <tannerbryant@gmail.com>
2018-06-05 11:36:07 -0700

    Second commit

--- assessment.js
+++ assessment.js
@@ -50,8 +50,11 @@ var responseCreator = function(param){
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
-// CODE HERE
+function sum(number) {
+  number = 5;
+  return number +=;
+};
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
-// CODE HERE
+function greeter();
